8128150e57f697170082b340d2e00e9b40e5afab
idranme <96647698+idranme@users.noreply.github.com>
2026-02-18 21:22:01 +0800

    fix(onebot): fixed an issue where the `owner_id` returned by `get_group_info` could be `0`

--- src/onebot11/action/group/GetGroupInfo.ts
+++ src/onebot11/action/group/GetGroupInfo.ts
@@ -25,7 +25,7 @@ class GetGroupInfo extends BaseAction<Payload, OB11Group> {
       remark_name: groupDetail.remarkName,
       avatar_url: `https://p.qlogo.cn/gh/${groupDetail.groupCode}/${groupDetail.groupCode}/0`,
       // 以下是非 OneBot 11 标准字段
-      owner_id: +groupDetail.ownerUin,  // 群主 QQ 号
+      owner_id: +groupDetail.ownerUin || +(await this.ctx.ntUserApi.getUinByUid(groupDetail.ownerUid)),  // 群主 QQ 号
       is_top: groupDetail.isTop,  // 是否置顶群聊
       shut_up_all_timestamp: groupDetail.shutUpAllTimestamp,  // 群全员禁言截止时间
       shut_up_me_timestamp: groupDetail.shutUpMeTimestamp,  // 我被禁言截止时间
